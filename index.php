7abcea13ae5c82059dfc8e013745f7dc885f2fbb
Rashid NHM <nhmrashid@gmail.com>
2017-09-23 11:15:10 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="style.css">
+<div class="console">
+	<div class="output">
+		<span>Connecting to terminal student@rbc.ca</span><br/>
+		<span class="green">Connection successful!</span><br/>
+		<span class="seperator">== == == == == == == == == == == == == == == == == ==</span></br>
+		<pre contenteditable="false" style="color: white;"></pre></br>
+		<span class="seperator">== == == == == == == == == == == == == == == == == ==</span></br>
+		<span>Hope you have fun discovering all the <span class="red">hidden gems</span>!</span>
+		</br>
+		<span class="blue">type '<span class="grey">help</span>' to see a list of comands available</span><br>
+	</div>
+	<div class="action">
+		<span>dev$ </span>
+		<textarea class="input" name="input" cols="30" rows="1"></textarea>
+	</div>
+</div>
